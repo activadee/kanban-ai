7375381b722be28bc3024cf31b5bc9a535f4b9af
Patryk Kalwak <pkalwak@gmx.de>
2025-10-11 15:14:40 +0200

    Update client/src/components/icons/SimpleIcons.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- client/src/components/icons/SimpleIcons.tsx
+++ client/src/components/icons/SimpleIcons.tsx
@@ -4,7 +4,9 @@ import {SiGithub} from '@icons-pack/react-simple-icons'
 type SimpleIconProps = ComponentProps<typeof SiGithub>
 
 export function GitHubIcon({title, ...props}: SimpleIconProps) {
-    return <SiGithub title={title} {...props}/>
+export function GitHubIcon({title, color = 'currentColor', ...props}: SimpleIconProps) {
+    return <SiGithub title={title} color={color} {...props}/>
+}
 }
 
 export type {SimpleIconProps}
