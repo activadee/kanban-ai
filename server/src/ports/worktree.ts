3f5ac5d01829286e260223da4fc0df89a07f3e91
Patryk Kalwak <pkalwak@gmx.de>
2025-11-26 13:30:16 +0100

    [KA-47] Server: refine filesystem and worktree modules (#131)

--- server/src/ports/worktree.ts
+++ server/src/ports/worktree.ts
@@ -1,20 +1,12 @@
 import {setWorktreeProvider} from 'core'
-import {createWorktree as srvCreate, removeWorktree as srvRemove} from '../fs/worktree'
-import {getWorktreePath as p1, getWorktreePathByNames as p2} from '../fs/paths'
+import {createWorktree, removeWorktree} from '../fs/worktree-runner'
+import {getWorktreePath, getWorktreePathByNames} from '../fs/paths'
 
 export function registerWorktreeProvider() {
     setWorktreeProvider({
-        async createWorktree(repoPath, base, branch, outDir, meta) {
-            return srvCreate(repoPath, base, branch, outDir, meta)
-        },
-        async removeWorktree(repoPath, worktreePath, meta) {
-            return srvRemove(repoPath, worktreePath, meta)
-        },
-        getWorktreePath(projectId, attemptId) {
-            return p1(projectId, attemptId)
-        },
-        getWorktreePathByNames(projectName, taskName) {
-            return p2(projectName, taskName)
-        },
+        createWorktree,
+        removeWorktree,
+        getWorktreePath,
+        getWorktreePathByNames,
     })
 }
