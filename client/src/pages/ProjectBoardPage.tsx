8b2ac30e81ab5792d0205491c0b2ea3343f37c48
activadee <pkalwak@gmx.de>
2026-01-02 03:12:04 +0100

    Remove unused sortOrder prop from Board component

--- client/src/pages/ProjectBoardPage.tsx
+++ client/src/pages/ProjectBoardPage.tsx
@@ -317,7 +317,6 @@ export function ProjectBoardPage() {
                         projectId={project.id}
                         state={boardState}
                         initialSelectedCardId={initialSelectedCardId}
-                        sortOrder={sortOrder}
                         enhancementStatusByCardId={
                             enhancementStatusByCardId
                         }
